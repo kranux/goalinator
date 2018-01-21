2951e8d94bd7d75625c6ed577c7abb7226ea580a
Andrius Kripaitis <andrius.kripaitis@gmail.com>
2018-01-21 15:48:06 +0200

    Improved progress calculation;

--- src/components/goal-list-item/index.js
+++ src/components/goal-list-item/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
 
-import {formatTime} from '../../utils';
+import {
+  formatTime,
+  formatPercent,
+  calculateDifferenceInDays
+} from '../../utils';
+
+const calculateProgressInPercent = goal => {
+  const lambda = goal.startValue - goal.value;
+  const goalLambda = goal.startValue - goal.goalValue;
+  return lambda / goalLambda * 100;
+}
+
+const calculateDaysLeft = goal =>
+  calculateDifferenceInDays(goal.startTime, goal.goalTime);
 
 export default class GoalListItem extends React.Component {
 
   render() {
     const goal = this.props.goal;
+
     return  (
       <li>
-        <span className="name">{goal.name}</span>
-        <span className="values">{goal.startValue} &#x2192; {goal.goalValue}</span>
-        <span className="time">{formatTime(goal.startTime)} {formatTime(goal.goalTime)}</span>
-        <ul className="controlls">
+        <span className="name">
+          {goal.name}
+        </span>
+        <span className="progress">
+          Progress: {formatPercent(calculateProgressInPercent(goal))}%
+        </span>
+        <span className="daysLeft">
+          {calculateDaysLeft(goal)} days to go
+        </span>
+        <ul>
+          <span className="values">
+            Current: {goal.value}
+            {goal.startValue} &#x2192; {goal.goalValue}
+          </span>
+          <span className="time">{formatTime(goal.startTime)} {formatTime(goal.goalTime)}</span>
           <li>
             <a onClick={() => {this.props.showProgress(goal)}}>Show progress</a>
           </li>
--- src/utils.js
+++ src/utils.js
@@ -2,4 +2,10 @@ import moment from 'moment';
 
 export const DATE_FORMAT = 'YYYY-MM-DD';
 
-export const formatTime = (time, format) => moment(time).format(format || DATE_FORMAT);
+export const formatTime = (time, format) =>
+  moment(time).format(format || DATE_FORMAT);
+
+export const calculateDifferenceInDays = (a, b) =>
+  (console.log(a, b), moment(b).diff(moment(a), 'days'));
+
+export const formatPercent = value => value.toFixed(2);
