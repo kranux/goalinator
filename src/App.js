a7eadafa7d35b6b1c17064a24a3e2995743a2a75
Andrius Kripaitis <andrius.kripaitis@gmail.com>
2018-01-21 12:46:36 +0200

    Added more fields to add Element form;

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import moment from 'moment';
+
+export const DATE_FORMAT = 'YYYY-MM-DD';
+
+export const formatTime = (time, format) => moment(time).format(format || DATE_FORMAT);
+
+const formItem = (label, input) => (
+  <div className="form-item">
+    <label>{label}</label>
+    {input}
+  </div>
+);
+
 class App extends Component {
 
   constructor(props) {
@@ -38,7 +51,11 @@ class App extends Component {
   showAddGoal() {
     this.setState({
       newGoalForm: {
-
+        name: '',
+        startlValue: 0,
+        startTime: new Date(),
+        goalDate: undefined,
+        goalValue: 0
       }
     });
   }
@@ -54,12 +71,41 @@ class App extends Component {
         <button onClick={this.showAddGoal}>Add Goal</button>
         {this.state.newGoalForm ? <div className="dialog">
           <form onSubmit={this.handleSubmit}>
-            <input 
-              name="name"
-              onChange={this.handleChange}
-              type="text"
-              value={this.state.newGoalForm.name}
-            />
+            {formItem('Name', 
+              <input 
+                name="name"
+                onChange={this.handleChange}
+                type="text"
+                value={this.state.newGoalForm.name}
+              />)}
+            {formItem('Start value', 
+              <input
+                name="startValue"
+                onChange={this.handleChange}
+                type="number"
+                value={this.state.newGoalForm.startValue}
+              />)}
+            {formItem('Start date',
+              <input
+                name="startTime"
+                onChange={this.handleChange}
+                type="date"
+                value={formatTime(this.state.newGoalForm.startTime)}
+              />)}
+            {formItem('Goal value', 
+              <input
+                name="goalValue"
+                onChange={this.handleChange}
+                type="number"
+                value={this.state.newGoalForm.goalValue}
+              />)}
+            {formItem('Goal date',
+              <input
+                name="goalTime"
+                onChange={this.handleChange}
+                type="date"
+                value={formatTime(this.state.newGoalForm.goalTime)}
+              />)}
             <button type="submit">Add</button>
           </form>
         </div> : null}
