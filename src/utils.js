a0071253e3360d622cd5e1005292694e77133369
Andrius Kripaitis <andrius.kripaitis@gmail.com>
2018-01-21 15:51:08 +0200

    Clenup;

--- src/utils.js
+++ src/utils.js
@@ -6,6 +6,6 @@ export const formatTime = (time, format) =>
   moment(time).format(format || DATE_FORMAT);
 
 export const calculateDifferenceInDays = (a, b) =>
-  (console.log(a, b), moment(b).diff(moment(a), 'days'));
+  moment(b).diff(moment(a), 'days');
 
 export const formatPercent = value => value.toFixed(2);
